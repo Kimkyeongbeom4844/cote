4916e334c21f949ed66359b8a5ac07694ed67b68
김경범 <xops09685@gmail.com>
2023-03-30 14:17:44 +0900

    [unrated] Title: 둘만의 암호, Time: 4.87 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
@@ -0,0 +1,26 @@
+const solution = (s, skip, index) => {
+    const sArr = [...s].map(v=>v.charCodeAt())
+    const skipArr = [...skip].map(v=>v.charCodeAt())
+    const numList = Array.from(Array(26),(v,i)=>i+97).filter(v=>!skipArr.includes(v));
+    console.log(sArr,skipArr,numList)
+    return sArr.reduce((a,c,i)=>{
+        console.log(numList.indexOf(c)+index, numList.length-1)
+        if(numList.indexOf(c)+index > numList.length-1){
+            return [...a,String.fromCharCode(numList[(numList.indexOf(c)+index)%numList.length])]
+        }
+        return [...a,String.fromCharCode(numList[numList.indexOf(c)+index])]
+    },[]).join('')
+}
+// const solution = (s, skip, index) => {
+//   const sArr = [...s].map(v => v.charCodeAt());
+//   const skipArr = [...skip].map(v => v.charCodeAt());
+//   const numList = Array.from(Array(26), (_, i) => i + 97).filter(v => !skipArr.includes(v));
+
+//   return sArr.reduce((a, c, i) => {
+//     if (skipArr.includes(c)) {
+//       return [...a, s[i]];
+//     }
+//     const charCode = numList[(numList.indexOf(c) + index) % numList.length];
+//     return [...a, String.fromCharCode(charCode)];
+//   }, []).join('');
+// };
