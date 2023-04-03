dd6f8a714984c9b62151c88eaa7b27ece103b181
김경범 <xops09685@gmail.com>
2023-04-03 15:47:27 +0900

    Update 공원 산책.js

--- 프로그래머스/unrated/172928. 공원 산책/공원 산책.js
+++ 프로그래머스/unrated/172928. 공원 산책/공원 산책.js
@@ -4,53 +4,40 @@ const solution = (park, routes) => {
     let start = null;
     for(let i=0; i<arr.length; i++){
         if(arr[i].includes('S')){
-            start = [i,arr[i].indexOf('S')]
+            start = [i,arr[i].indexOf('S')];
             break;
         }
     };
-    // console.log(start);
     for(let i=0; i<routes.length; i++){
         const [방향, 무빙] = routes[i].split(' ');
         const [좌,우] = start;
         switch(방향){
             case 'E' :
                 for(let j=1; j<=Number(무빙); j++){
-                    if(arr[좌]?.[우+j] === 'X' || arr[좌]?.[우+j] === undefined){
-                        break;
-                    } else {
-                        if(j === Number(무빙)) start[1] += Number(무빙);
-                    }
+                    if(arr[좌][우+j] === 'X' || arr[좌]?.[우+j] === undefined) break;
+                    else if(j === Number(무빙)) start[1] += Number(무빙);
                 }
                 break;
             case 'W' :
                 for(let j=1; j<=Number(무빙); j++){
-                    if(arr[좌]?.[우-j] === 'X' || arr[좌]?.[우-j] === undefined){
-                        break;
-                    } else {
-                        if(j === Number(무빙)) start[1] -= Number(무빙);
-                    }
+                    if(arr[좌][우-j] === 'X' || arr[좌]?.[우-j] === undefined) break;
+                    else if(j === Number(무빙)) start[1] -= Number(무빙);
                 }
                 break;
             case 'S' :
                 for(let j=1; j<=Number(무빙); j++){
-                    if(arr[좌+j]?.[우] === 'X' || arr[좌+j]?.[우] === undefined){
-                        break;
-                    } else {
-                        if(j === Number(무빙)) start[0] += Number(무빙);
-                    }
+                    if(arr[좌+j]?.[우] === 'X' || arr[좌+j]?.[우] === undefined) break;
+                    else if(j === Number(무빙)) start[0] += Number(무빙);
                 }
                 break;
             case 'N' :
                 for(let j=1; j<=Number(무빙); j++){
-                    if(arr[좌-j]?.[우] === 'X' || arr[좌-j]?.[우] === undefined){
-                        break;
-                    } else {
-                        if(j === Number(무빙)) start[0] -= Number(무빙);
-                    }
+                    if(arr[좌-j]?.[우] === 'X' || arr[좌-j]?.[우] === undefined) break;
+                    else if(j === Number(무빙)) start[0] -= Number(무빙);
                 }
                 break;
         }
         // console.log(start);
     }
-    return start
+    return start;
 }
