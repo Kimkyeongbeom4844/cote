c0fefe5cecb816b325a901595ed03d8d14b8f436
김경범 <xops09685@gmail.com>
2023-04-04 16:27:18 +0900

    [level 1] Title: 실패율, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/README.md
+++ 프로그래머스/lv1/42889. 실패율/README.md
@@ -1,10 +1,10 @@
 # [level 1] 실패율 - 42889 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 2.93 ms
+메모리: 0.0 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-<br/>정확성: 100.0<br/>합계: 100.0 / 100.0
+Empty
 
 ### 문제 설명
 
--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -4,17 +4,12 @@ const solution = (N, stages) => {
   let remainPeople;
 
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== arr[i - 1]){
-        if(result[arr[i-1] - 1]?.[1] !== undefined){
-            result[arr[i-1] - 1][1] = result[arr[i-1] - 1][1]/remainPeople
-        }
-        remainPeople = arr.length - i;
-    }
+    if (arr[i] !== arr[i - 1]) remainPeople = arr.length - i;
     if (result[arr[i] - 1]?.[1] !== undefined){
-        result[arr[i] - 1][1] += 1;
+        result[arr[i] - 1][1] += (1 / remainPeople);
     } 
   }
-  console.log(result);
+    console.log(result);
   return result.sort((a, b) =>{
       if (a[1] === b[1]) return a[0] - b[0];
       else return b[1] - a[1];
