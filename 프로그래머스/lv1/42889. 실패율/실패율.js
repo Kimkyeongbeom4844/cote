ead31ac4eac783a0d0b240654d9afe198b447ffe
김경범 <xops09685@gmail.com>
2023-04-04 16:31:48 +0900

    [level 1] Title: 실패율, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -16,3 +16,17 @@ const solution = (N, stages) => {
       else return b[1] - a[1];
   }).map((v) => v[0]);
 };
+// function solution(N, stages) {
+//   const failRates = Array.from(Array(N), (v, i) => {
+//     const 분모 = stages.filter((stage) => stage >= i + 1).length;
+//     const 분자 = stages.filter((stage) => stage === i + 1).length;
+//     return { stage: i + 1, failRate: 분자 / 분모 || 0 };
+//   });
+//   console.log(failRates)
+//   failRates.sort((a, b) => b.failRate - a.failRate || a.stage - b.stage);
+  
+//   return failRates.map((failRate) => failRate.stage);
+// }
+
+
+
