f5ec0cb4fadf864449e70b3f6a91eb8b35152c59
김경범 <xops09685@gmail.com>
2023-04-04 16:29:28 +0900

    Update 실패율.js

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -4,28 +4,15 @@ const solution = (N, stages) => {
   let remainPeople;
 
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== arr[i - 1]) remainPeople = arr.length - i;
-    if (result[arr[i] - 1]?.[1] !== undefined){
-        result[arr[i] - 1][1] += (1 / remainPeople);
-    } 
+    if (arr[i] !== arr[i - 1]){
+        if(result[arr[i-1] - 1]?.[1] !== undefined) result[arr[i-1] - 1][1] = result[arr[i-1] - 1][1]/remainPeople;
+        remainPeople = arr.length - i;
+    }
+    if (result[arr[i] - 1]?.[1] !== undefined) result[arr[i] - 1][1] += 1;
   }
-    console.log(result);
+  // console.log(result);
   return result.sort((a, b) =>{
       if (a[1] === b[1]) return a[0] - b[0];
       else return b[1] - a[1];
   }).map((v) => v[0]);
 };
-// function solution(N, stages) {
-//   const failRates = Array.from(Array(N), (v, i) => {
-//     const 분모 = stages.filter((stage) => stage >= i + 1).length;
-//     const 분자 = stages.filter((stage) => stage === i + 1).length;
-//     return { stage: i + 1, failRate: 분자 / 분모 || 0 };
-//   });
-//   console.log(failRates)
-//   failRates.sort((a, b) => b.failRate - a.failRate || a.stage - b.stage);
-  
-//   return failRates.map((failRate) => failRate.stage);
-// }
-
-
-
