6cb83581d8e8d87eb1cc611e2a161a5f0163523b
김경범 <xops09685@gmail.com>
2023-04-12 12:00:56 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 86.78 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -3,16 +3,10 @@ const solution = (elements) => {
     for(let i=0; i<elements.length; i++){
         let sum = 0;
         for(let j=i; j<i+(elements.length-1); j++){
-            if(elements[j]){
-                // console.log(j);
-                sum += elements[j];
-            } 
-            else{
-                // console.log(j-elements.length);
-                sum += elements[j-elements.length]
-            }
+            if(elements[j]) sum += elements[j];
+            else sum += elements[j-elements.length]
             set.add(sum);
         }
     }
-    return set.size+1
+    return set.size+1;
 }
