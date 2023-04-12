b241b483c0e17451a5614149ffb9e5620d24a179
김경범 <xops09685@gmail.com>
2023-04-12 14:55:15 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.40 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -1,14 +1,12 @@
 const solution = (str1, str2) => {
-    const reqExp = /[^A-Za-z]/;
+    const reqExp = /[^A-Z]/i;
     const 쌍분리 = (str) => {
         const arr = [];
         let start = 0; 
         let end = 1;
         do {
             const word = str[start].concat(str[end]);
-            if(!reqExp.test(word)){
-                arr.push(word)
-            } 
+            if(!reqExp.test(word)) arr.push(word);
             start++;
             end++;
         } while(end !== str.length);
@@ -22,20 +20,20 @@ const solution = (str1, str2) => {
     const map1 = 쌍분리(str1.toUpperCase());
     const map2 = 쌍분리(str2.toUpperCase());
     // console.log(map1,map2)
-    let sum = 0;
-    let 교집합 = 0;
-    const filterArr = [...new Set([...map1.keys(),...map2.keys()])]
-    for(let i=0; i<filterArr.length; i++){
-        // console.log(map1.get(filterArr[i])??0,map2.get(filterArr[i])??0)
-        const min = Math.min(...[map1.get(filterArr[i])??0,map2.get(filterArr[i])??0])
-        const max = Math.max(...[map1.get(filterArr[i])??0,map2.get(filterArr[i])??0])
-        if(min === 0) sum += max;
+    let 분모 = 0;
+    let 분자 = 0;
+    const mapSumArr = [...new Set([...map1.keys(),...map2.keys()])]
+    for(let i=0; i<mapSumArr.length; i++){
+        // console.log(map1.get(mapSumArr[i])??0,map2.get(mapSumArr[i])??0)
+        const min = Math.min(...[map1.get(mapSumArr[i])??0,map2.get(mapSumArr[i])??0])
+        const max = Math.max(...[map1.get(mapSumArr[i])??0,map2.get(mapSumArr[i])??0])
+        if(min === 0) 분모 += max;
         else {
-            교집합 += min;
-            sum+=max;
+            분자 += min;
+            분모 += max;
         }
     }
-    // console.log(교집합,sum)
-    if(교집합 === 0 && sum === 0) return 65536;
-    else return parseInt(교집합/sum * 65536);
+    // console.log(분자,분모)
+    if(분자 === 0 && 분모 === 0) return 65536;
+    else return parseInt(분자/분모 * 65536);
 }
