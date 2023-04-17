97e890e271a3309c81b904d6d70de279eb7e0a0a
김경범 <xops09685@gmail.com>
2023-04-17 19:30:03 +0900

    [level 3] Title: 야근 지수, Time: 7.89 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/lv3/12927. 야근 지수/야근 지수.js
+++ 프로그래머스/lv3/12927. 야근 지수/야근 지수.js
@@ -1,6 +1,6 @@
 const solution = (n, works) => {
     const arr = [...works].sort((a,b)=>b-a);
-    console.log(arr);
+    // console.log(arr);
     while(n > 0){
         let index = 0;
         let count = 0;
@@ -8,7 +8,7 @@ const solution = (n, works) => {
             count++;
             index++;
         }
-        // console.log(count,++index);
+        // console.log(count);
         for(let i=0; i<=count; i++){
             arr[i]--;
             if(arr[i] === -1) return 0;
