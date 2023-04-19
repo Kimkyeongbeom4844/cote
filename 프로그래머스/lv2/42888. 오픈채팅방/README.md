1b368f9b16cea630afe9c1dd8fb801345306862b
김경범 <xops09685@gmail.com>
2023-04-19 19:34:26 +0900

    [level 2] Title: 오픈채팅방, Time: 182.55 ms, Memory: 96.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42888. 오픈채팅방/README.md
+++ 프로그래머스/lv2/42888. 오픈채팅방/README.md
@@ -1,10 +1,10 @@
 # [level 2] 오픈채팅방 - 42888 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42888) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42888#qna) 
 
 ### 성능 요약
 
-메모리: 97.3 MB, 시간: 118.77 ms
+메모리: 96.9 MB, 시간: 182.55 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.js
@@ -1,4 +1,54 @@
-function solution(record) {
-    var answer = [];
-    return answer;
+const solution = (record) => {
+    const result = [];
+    const map = new Map();
+    for(let i=0; i<record.length; i++){
+        const [type,uid,name] = record[i].split(' ');
+        switch(type){
+            case 'Enter':
+                map.set(uid,name);
+                result.push({type, uid});
+                break;
+            case 'Leave':
+                result.push({type , uid});
+                break;
+            case 'Change':
+                map.set(uid,name);
+                break;
+        }
+    }
+    // console.log(result,map)
+    
+    // reduce => 시간초과
+    // return result.reduce((a,c)=>{
+    //     switch(c.type){
+    //         case 'Enter':
+    //             return [...a,`${map.get(c.uid)}님이 들어왔습니다.`];
+    //         case 'Leave':
+    //             return [...a,`${map.get(c.uid)}님이 나갔습니다.`];
+    //     }
+    // },[])
+    
+    //map => 통과
+    // return result.map((v,i)=>{
+    //     switch(v.type){
+    //         case 'Enter':
+    //             return `${map.get(v.uid)}님이 들어왔습니다.`;
+    //         case 'Leave':
+    //             return `${map.get(v.uid)}님이 나갔습니다.`;
+    //     }
+    // })
+    
+    //for문 => 통과
+    const arr = [];
+    for(let i=0; i<result.length; i++){
+        switch(result[i].type){
+            case 'Enter':
+                arr.push(`${map.get(result[i].uid)}님이 들어왔습니다.`);
+                break;
+            case 'Leave':
+                arr.push(`${map.get(result[i].uid)}님이 나갔습니다.`);
+                break;
+        }
+    }
+    return arr;
 }
