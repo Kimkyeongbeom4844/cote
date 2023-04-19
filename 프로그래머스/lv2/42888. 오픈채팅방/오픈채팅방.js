adcbb7d976662554aa21495a1945848ea7a0a247
김경범 <xops09685@gmail.com>
2023-04-19 19:29:56 +0900

    [level 2] Title: 오픈채팅방, Time: 118.77 ms, Memory: 97.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.js
@@ -1,28 +1,4 @@
-const solution = (record) => {
-    const result = [];
-    const map = new Map();
-    for(let i=0; i<record.length; i++){
-        const [type,uid,name] = record[i].split(' ');
-        switch(type){
-            case 'Enter':
-                map.set(uid,name);
-                result.push({type, uid});
-                break;
-            case 'Leave':
-                result.push({type , uid});
-                break;
-            case 'Change':
-                map.set(uid,name);
-                break;
-        }
-    }
-    // console.log(result,map)
-    return result.map((v,i)=>{
-        switch(v.type){
-            case 'Enter':
-                return `${map.get(v.uid)}님이 들어왔습니다.`;
-            case 'Leave':
-                return `${map.get(v.uid)}님이 나갔습니다.`;
-        }
-    })
+function solution(record) {
+    var answer = [];
+    return answer;
 }
