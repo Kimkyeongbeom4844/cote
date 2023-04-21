ce4f57aff024bda08321fd754e08a1267eae3d97
김경범 <xops09685@gmail.com>
2023-04-21 15:30:19 +0900

    [unrated] Title: 할 일 목록, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.js
@@ -0,0 +1,7 @@
+const solution = (todo_list, finished) => {
+    const result = [];
+    for(let i=0; i<finished.length; i++){
+        if(!finished[i]) result.push(todo_list[i])
+    }
+    return result;
+}
