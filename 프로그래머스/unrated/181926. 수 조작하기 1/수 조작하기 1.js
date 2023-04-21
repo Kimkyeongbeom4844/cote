180e16ad603bc1b6338f996e5711223ed715f5d0
김경범 <xops09685@gmail.com>
2023-04-21 12:28:10 +0900

    [unrated] Title: 수 조작하기 1, Time: 2.47 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,20 @@
+const solution = (n, control) => {
+    let result = n;
+    for(let i=0; i<control.length; i++){
+        switch(control[i]){
+            case 'w':
+                result += 1;
+                break;
+            case 's':
+                result -= 1;
+                break;
+            case 'd':
+                result += 10;
+                break;
+            case 'a':
+                result -= 10;
+                break;
+        }
+    }
+    return result;
+}
