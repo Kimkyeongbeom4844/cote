c8b5cc095a39de634230e1d9b7f2ed7aa44a1516
김경범 <xops09685@gmail.com>
2023-04-24 17:29:44 +0900

    [unrated] Title: 배열 만들기 2, Time: 8.18 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
@@ -1,13 +1,11 @@
 const solution = (l, r) => {
     const result = [];
     for(let i=l; i<=r; i++){
-        const regExp = /0|5/
-        const word = i.toString();
-        let isPass = true;
-        for(let j=0; j<word.length; j++){
-            if(!regExp.test(word[j])) isPass = false;
-        }
-        if(isPass) result.push(i);
+        const regExp = /^[05]+$/
+        if(regExp.test(i)){
+            // console.log(i);
+            result.push(i)
+        } 
     }
     if(result.length) return result;
     return [-1];
