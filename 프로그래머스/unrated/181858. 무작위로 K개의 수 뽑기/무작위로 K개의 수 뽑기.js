0f9241333bc9763f42aba328c71c616ae5b38f3e
김경범 <xops09685@gmail.com>
2023-04-24 14:10:41 +0900

    [unrated] Title: 무작위로 K개의 수 뽑기, Time: 1.94 ms, Memory: 35.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
@@ -0,0 +1,8 @@
+const solution = (arr, k) => {
+    const set = new Set(arr);
+    const result = [...set].slice(0,k);
+    while(result.length !== k){
+        result.push(-1);
+    }
+    return result;
+}
