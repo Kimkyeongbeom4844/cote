d720cb6c7cfddd72f894a66d879de97c12cf284f
김경범 <xops09685@gmail.com>
2023-05-08 10:40:13 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 11.96 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -1,25 +1,19 @@
 const solution = (files) => {
-    const headArr = [];
     const arr = [];
     for(let i=0; i<files.length; i++){
         const NUMBER = files[i].match(/[0-9]+/)[0]
         const word = files[i].split(NUMBER)
         const HEAD = word[0];
         const TAIL = word[1];
-        headArr.push([HEAD.toUpperCase(),i]);
         arr.push([HEAD.toUpperCase(),Number(NUMBER),TAIL,i])
     }
-    const result = arr.sort((a,b)=>{
-        if(a[0].localeCompare(b[0]) === 0){
-            return a[1]-b[1]
-        }
+    const sortArr = arr.sort((a,b)=>{
+        if(a[0].localeCompare(b[0]) === 0) return a[1]-b[1]
         return a[0].localeCompare(b[0]);
     })
-    // console.log(result)
-    const res = [];
-    for(let i=0; i<result.length; i++){
-        res.push(files[result[i][3]])  
+    const result = [];
+    for(let i=0; i<sortArr.length; i++){
+        result.push(files[sortArr[i][3]])  
     }
-    // console.log(res)
-    return res;
+    return result;
 }
