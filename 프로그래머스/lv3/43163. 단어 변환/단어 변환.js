edd26b944037581a4934cbba8ffa4560a6b840bb
김경범 <xops09685@gmail.com>
2023-05-09 14:09:39 +0900

    [level 3] Title: 단어 변환, Time: 4.97 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.js
@@ -0,0 +1,26 @@
+const solution = (begin, target, words) => {
+    const set = new Set();
+    const reqFuc = (ARR) => {
+        const curWord = ARR[ARR.length-1];
+        const remainArr = words.filter(v=>!ARR.includes(v));
+        // console.log(remainArr);
+        let deathCount = 0;
+        for(let i=0; i<remainArr.length; i++){
+            let count = 0;
+            for(let j=0; j<remainArr[i].length; j++){
+                if(remainArr[i][j] !== curWord[j]) count++;
+            }
+            if(count === 1){
+                if(remainArr[i] === target) return set.add(ARR.length);
+                else reqFuc([...ARR,remainArr[i]]);
+            } 
+            else deathCount++;
+        }
+        if(deathCount === remainArr.length) return set.add(-1);
+    }
+    reqFuc([begin]);
+    console.log(set)
+    set.delete(-1);
+    if(set.size) return Math.min(...set);
+    return 0;
+}
