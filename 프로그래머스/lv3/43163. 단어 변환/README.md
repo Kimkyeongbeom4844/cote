4bca291a692e568f07a40c392d2d8f4989e087f1
김경범 <xops09685@gmail.com>
2023-05-09 14:11:22 +0900

    [level 3] Title: 단어 변환, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/README.md
+++ 프로그래머스/lv3/43163. 단어 변환/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163) 
 
 ### 성능 요약
 
-메모리: 33.7 MB, 시간: 4.97 ms
+메모리: 33.4 MB, 시간: 0.18 ms
 
 ### 구분
 
--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.js
@@ -3,7 +3,6 @@ const solution = (begin, target, words) => {
     const reqFuc = (ARR) => {
         const curWord = ARR[ARR.length-1];
         const remainArr = words.filter(v=>!ARR.includes(v));
-        // console.log(remainArr);
         let deathCount = 0;
         for(let i=0; i<remainArr.length; i++){
             let count = 0;
@@ -11,15 +10,15 @@ const solution = (begin, target, words) => {
                 if(remainArr[i][j] !== curWord[j]) count++;
             }
             if(count === 1){
-                if(remainArr[i] === target) return set.add(ARR.length);
+                if(remainArr[i] === target) set.add(ARR.length);
                 else reqFuc([...ARR,remainArr[i]]);
             } 
             else deathCount++;
         }
-        if(deathCount === remainArr.length) return set.add(-1);
+        if(deathCount === remainArr.length) set.add(-1);
     }
     reqFuc([begin]);
-    console.log(set)
+    // console.log(set)
     set.delete(-1);
     if(set.size) return Math.min(...set);
     return 0;
