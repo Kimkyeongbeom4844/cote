5942cf20557d4746545f17db21004b9e4923ce03
김경범 <xops09685@gmail.com>
2024-07-21 15:29:35 +0900

    [level 0] Title: 최빈값 구하기, Time: 5.12 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -0,0 +1,23 @@
+function solution(array) {
+    const obj = {};
+    for(let i=0; i<array.length; i++){
+        if(obj[array[i]] === undefined){
+            obj[array[i]] = 1;
+            continue;
+        }
+        obj[array[i]] += 1;
+    }
+    console.log(obj);
+    const arr = Object.entries(obj).sort((a,b)=>{
+        if(a[1]>b[1]){
+            return -1;
+        }
+        if(a[1]<b[1]){
+            return 1;
+        }
+        return 0;
+    })
+    if(arr.length === 1) return Number(arr[0][0])
+    if(arr[0][1] === arr[1][1]) return -1;
+    return Number(arr[0][0])
+}
